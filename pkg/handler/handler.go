e2119a40cd0f5d3633c1b0ecd1f4e9520a3810a5
Namdar1Ibrakhim <namdaribrahim04@gmail.com>
2024-09-25 17:09:06 +0500

    update handle

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -15,51 +15,32 @@ func NewHandler(services *service.Service) *Handler {
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
-	rout	auth := router.Group("/auth")
+	router := gin.New()
+
+	auth := router.Group("/auth")
 	{
 		authStudent := auth.Group("/student")
 		{
 			authStudent.POST("/sign-up", h.signUpStudent)
 			authStudent.POST("/sign-in", h.signIn) // один метод для всех полей
 		}
-		authInstructor := auth.Group("/instructor")
+		instructorRoutes := router.Group("/instructor")
 		{
-			authInstructor.POST("/sign-up", h.signUpInstructor)
-			authInstructor.POST("/sign-in", h.signIn) // один метод для всех полей
+			instructorRoutes.POST("/sign-up", h.signUpInstructor)
+			instructorRoutes.POST("/sign-in", h.signIn)            // один метод для всех полей
+			instructorRoutes.GET("/student/:id", h.getStudentById) // один метод для всех полей
+
 		}
-		authAdmin := auth.Group("/admin")
+
+		adminRoutes := router.Group("/admin")
 		{
-			authAdmin.POST("/sign-up", h.signUpAdmin)
-			authAdmin.POST("/sign-in", h.signIn) // один метод для всех полей
+			adminRoutes.POST("/sign-up", h.signUpAdmin)
+			adminRoutes.POST("/sign-in", h.signIn) // один метод для всех полей
+			adminRoutes.GET("/user/:id", h.getUserById)
+
 		}
-		auth.GET("/get", h.getUser)
 		//authUser.PUT("/update", h.updateUser)
 		//authUser.DELETE("/delete", h.updateUser)
-	}
-
-<<<<<<< HEAD
-	instructorRoutes := router.Group("/instructor")
-	{
-		instructorRoutes.POST("/sign-up", h.signUpInstrucor)
-		instructorRoutes.POST("/sign-in", h.signIn) // один метод для всех полей
-		instructorRoutes.GET("/student/:id", h.getStudentById) // один метод для всех полей
-
-	}
-
-	adminRoutes := router.Group("/admin")
-	{
-		adminRoutes.POST("/sign-up", h.signUpAdmin)
-		adminRoutes.POST("/sign-in", h.signIn) // один метод для всех полей
-		adminRoutes.GET("/user/:id", h.getUserById)
-		
-
-	}
-
-=======
->>>>>>> bd88000fb7bc59bca576ee76a0a0de4ebfcf3a03
-GET("/user/:id", h.getUserById)
-		
-
 	}
 
 	return router
