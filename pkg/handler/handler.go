2ae5c59d4490957e888b040c0738ad474e7e9d27
Namdar1Ibrakhim <namdaribrahim04@gmail.com>
2024-09-26 11:15:41 +0500

    update

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -37,7 +37,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			adminRoutes.POST("/sign-up", h.signUpAdmin)
 			adminRoutes.POST("/sign-in", h.signIn)      // один метод для всех полей
 			adminRoutes.GET("/user/:id", h.getUserById) // получение пользователя по айди
-			adminRoutes.GET("/editPassword/:id", h.editPasswordByUserId)
+			adminRoutes.GET("/editPassword/:id/:password", h.editPasswordByUserId)
 		}
 
 	}
@@ -47,7 +47,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	{
 		profile.PUT("/users/:id", h.UpdateUser)
 		profile.DELETE("/users/:id", h.DeleteUser) //удаление акк через админ и юзер
-		profile.GET("/editPassword/:id/:password", h.editPasswordByUserId)
+		profile.GET("/editPassword/:password", h.editPasswordByCurrentUserId)
 
 	}
 
