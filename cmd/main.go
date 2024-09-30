d3401191e643636651e313f9df48c305f0954232
Namdar1Ibrakhim <namdaribrahim04@gmail.com>
2024-09-30 10:19:55 +0500

    add cors

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Namdar1Ibrakhim/student-track-system/pkg/handler"
 	"github.com/Namdar1Ibrakhim/student-track-system/pkg/repository"
 	"github.com/Namdar1Ibrakhim/student-track-system/pkg/service"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -24,7 +25,7 @@ func main() {
 		logrus.Fatalf("error loading env variables: %s", err.Error()) //Проверка на инициализацию .env файла
 	}
 
-	db, err := repository.NewPostgresDB(repository.Config{    //создаем обьект NewPostgresDB реопзитория и инициализируем туда Конфигурации
+	db, err := repository.NewPostgresDB(repository.Config{ //создаем обьект NewPostgresDB реопзитория и инициализируем туда Конфигурации
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
@@ -32,7 +33,7 @@ func main() {
 		//из .env файлы
 		Password: os.Getenv("DB_PASSWORD"),
 		//
-		
+
 		DBName:  viper.GetString("db.dbname"),
 		SSLMode: viper.GetString("db.sslmode"),
 	})
@@ -57,4 +58,20 @@ func initConfig() error { //Функция добавления конфигур
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
