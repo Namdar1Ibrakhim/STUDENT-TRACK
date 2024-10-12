b402a0c2fb535a072d66c849bd56fa71b83781c8
Daniyar Nurlanov <108024213+chotamkz@users.noreply.github.com>
2024-10-12 21:41:12 +0600

    rewrote response of predict method

--- pkg/dto/PredictionResponseDto.go
+++ pkg/dto/PredictionResponseDto.go
@@ -1,6 +1,8 @@
 package dto
 
 type PredictionResponseDto struct {
-	PredictedTrack string `json:"predicted_track"`
+	Id             int    `json:"-" db:"id"`
+	Direction_name string `json:"direction_name" binding:"required"`
+	Description    string `json:"description" binding:"required"`
 	StudentId      int    `json:"student_id"`
 }
